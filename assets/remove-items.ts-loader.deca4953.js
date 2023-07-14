dcbe5714523c1d62a7aedf28f90221972bdcaca0
Layla <54817230+laylahoogeveen@users.noreply.github.com>
2023-07-14 12:24:36 +0200

    Show custom 'save search' button in Vinted's UI by default

--- assets/remove-items.ts-loader.deca4953.js
+++ assets/remove-items.ts-loader.deca4953.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/remove-items.ts.e37d703c.js")
+      chrome.runtime.getURL("assets/remove-items.ts.f0ec7e7b.js")
     );
   })().catch(console.error);
 

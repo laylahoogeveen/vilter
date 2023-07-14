d9b257f88f103cca1907e9af90142b90d9b34723
Layla <54817230+laylahoogeveen@users.noreply.github.com>
2023-07-14 13:24:13 +0200

    Fix regex bug

--- assets/remove-items.ts-loader.ca11f5b0.js
+++ assets/remove-items.ts-loader.ca11f5b0.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/remove-items.ts.f0ec7e7b.js")
+      chrome.runtime.getURL("assets/remove-items.ts.28868878.js")
     );
   })().catch(console.error);
 
